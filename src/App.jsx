e8a1aa62ccdb61c15a98019b17ed8b8e625cdf76
kandelmilan <kandelr950p@gmail.com>
2026-03-18 09:40:51 +0545

    Added

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-purple-500 to-pink-500">
+      <h1 className="text-5xl font-bold text-white">Tailwind is working!</h1>
+    </div>
+  );
+}
+
+export default App;
