7c80563ad6e85dcd02e25279c4e5b58e74c22c2f
kandelmilan <kandelr950p@gmail.com>
2026-03-24 22:52:43 +0545

    Added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { CartProvider } from "./assets/CartContext.jsx";
 import Index from "./pages/Index.jsx";
 import MenuPage from "./pages/MenuPage.jsx";
 import CartPage from "./pages/CartPage.jsx";
-// import NotFound from "./pages/NotFound.jsx";
+
 
 const App = () => (
   <CartProvider>
@@ -12,7 +12,7 @@ const App = () => (
         <Route path="/" element={<Index />} />
         <Route path="/menu" element={<MenuPage />} />
         <Route path="/cart" element={<CartPage />} />
-        {/* <Route path="*" element={<NotFound />} /> */}
+
       </Routes>
     </BrowserRouter>
   </CartProvider>
