6f11be5e2e03058014bbeedbf39d117d875d1168
kandelmilan <kandelr950p@gmail.com>
2026-03-28 15:55:29 +0545

    Added

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; // make sure your Tailwind / global styles are imported
+import "./index.css";
 import { CartProvider } from "./assets/CartContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
